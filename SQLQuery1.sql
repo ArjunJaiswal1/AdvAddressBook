c166ef1b0b6ba6efea39d7fd205b2908455a815e
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-28 09:33:49 +0530

    [Arjun] Add ; Added the method of creating the details of the person

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,15 @@
 ----uc1 create AddressBookService--
 create database AddressBookService;
 use AddressBookService;
+
+---UC2 Ability To Create AddressBookService Table---
+create table Address_Book(
+FirstName varchar(30) not null,
+LastName varchar(20) not null,
+Address varchar(100) not null,
+City varchar(50) not null,
+State varchar(50) not null,
+Zipcode varchar(30) not null,
+PhoneNumber varchar(20) not null,
+Email varchar(50) not null
+);
