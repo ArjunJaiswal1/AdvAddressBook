57cf0b68cea7730a5e68e434ed4eeabe52acae96
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-03-01 16:53:19 +0530

    [Arjun] Add ; Added the method to insert new contact

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -13,3 +13,10 @@ Zipcode varchar(30) not null,
 PhoneNumber varchar(20) not null,
 Email varchar(50) not null
 );
+
+----UC3 Ability to insert new contact address-----
+
+insert into Address_Book (FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email) Values ('Arjun', 'jaiswal', 'Motihari', ' dhaka', ' purnahiya', '845303', '9821848065', 'arjunjaiswal8065@gmail.com');
+insert into Address_Book Values ('prakash', 'Alok', 'Govindpura', ' Banglore', ' karnataka', '560045', '7677803859', 'paras@gmail.com');
+insert into Address_Book Values ('Raja', 'Gurjar', 'Tilaknagar', ' Newdelhi', ' Sagarpur', '110045', '727581654', 'raja12@gmail.com');
+insert into Address_Book Values ('Golu', 'Raj', 'Khagariya', ' Betiyyah', ' Bihar', '845345', '7345819656', 'Goluraj@gmail.com');
